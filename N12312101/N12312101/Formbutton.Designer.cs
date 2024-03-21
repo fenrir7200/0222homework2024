dfcaeefaeea89a37d2220a7e79f831b493aeef8e
fenrir7200 <a099036123@gmail.com>
2024-03-21 21:14:05 +0800

    button0321

--- N12312101/N12312101/Formbutton.Designer.cs
+++ N12312101/N12312101/Formbutton.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.buttonclick = new System.Windows.Forms.Button();
+            this.button0321 = new System.Windows.Forms.Button();
+            this.label0321 = new System.Windows.Forms.Label();
+            this.labcount = new System.Windows.Forms.Label();
+            this.btncount = new System.Windows.Forms.Button();
+            this.labcount1 = new System.Windows.Forms.Label();
+            this.btncount1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonclick
@@ -41,20 +47,90 @@
             this.buttonclick.UseVisualStyleBackColor = true;
             this.buttonclick.Click += new System.EventHandler(this.buttonclick_Click);
             // 
+            // button0321
+            // 
+            this.button0321.Location = new System.Drawing.Point(149, 240);
+            this.button0321.Name = "button0321";
+            this.button0321.Size = new System.Drawing.Size(75, 23);
+            this.button0321.TabIndex = 1;
+            this.button0321.Text = "button0321";
+            this.button0321.UseVisualStyleBackColor = true;
+            this.button0321.Click += new System.EventHandler(this.button0321_Click);
+            // 
+            // label0321
+            // 
+            this.label0321.AutoSize = true;
+            this.label0321.Location = new System.Drawing.Point(172, 172);
+            this.label0321.Name = "label0321";
+            this.label0321.Size = new System.Drawing.Size(51, 12);
+            this.label0321.TabIndex = 2;
+            this.label0321.Text = "label0321";
+            // 
+            // labcount
+            // 
+            this.labcount.AutoSize = true;
+            this.labcount.Location = new System.Drawing.Point(601, 184);
+            this.labcount.Name = "labcount";
+            this.labcount.Size = new System.Drawing.Size(11, 12);
+            this.labcount.TabIndex = 4;
+            this.labcount.Text = "0";
+            // 
+            // btncount
+            // 
+            this.btncount.Location = new System.Drawing.Point(578, 252);
+            this.btncount.Name = "btncount";
+            this.btncount.Size = new System.Drawing.Size(75, 23);
+            this.btncount.TabIndex = 3;
+            this.btncount.Text = "加一";
+            this.btncount.UseVisualStyleBackColor = true;
+            this.btncount.Click += new System.EventHandler(this.btncount_Click);
+            // 
+            // labcount1
+            // 
+            this.labcount1.AutoSize = true;
+            this.labcount1.Location = new System.Drawing.Point(386, 180);
+            this.labcount1.Name = "labcount1";
+            this.labcount1.Size = new System.Drawing.Size(23, 12);
+            this.labcount1.TabIndex = 6;
+            this.labcount1.Text = "100";
+            // 
+            // btncount1
+            // 
+            this.btncount1.Location = new System.Drawing.Point(363, 248);
+            this.btncount1.Name = "btncount1";
+            this.btncount1.Size = new System.Drawing.Size(75, 23);
+            this.btncount1.TabIndex = 5;
+            this.btncount1.Text = "減一";
+            this.btncount1.UseVisualStyleBackColor = true;
+            this.btncount1.Click += new System.EventHandler(this.btncount1_Click);
+            // 
             // Formbutton
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labcount1);
+            this.Controls.Add(this.btncount1);
+            this.Controls.Add(this.labcount);
+            this.Controls.Add(this.btncount);
+            this.Controls.Add(this.label0321);
+            this.Controls.Add(this.button0321);
             this.Controls.Add(this.buttonclick);
             this.Name = "Formbutton";
             this.Text = "按鈕教學";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonclick;
+        private System.Windows.Forms.Button button0321;
+        private System.Windows.Forms.Label label0321;
+        private System.Windows.Forms.Label labcount;
+        private System.Windows.Forms.Button btncount;
+        private System.Windows.Forms.Label labcount1;
+        private System.Windows.Forms.Button btncount1;
     }
 }
--- N12312101/N12312101/Formbutton.cs
+++ N12312101/N12312101/Formbutton.cs
@@ -24,5 +24,26 @@ namespace N12312101
             else if (buttonclick.Text == "我已經被按過了")
                 buttonclick.Text = "按我";
         }
+
+        private void button0321_Click(object sender, EventArgs e)
+        {
+            label0321.Text = "下面的按鈕已被按";
+            label0321.Size = new System.Drawing.Size(100, 20);
+            label0321.BackColor = System.Drawing.Color.Green;
+        }
+
+        private void btncount_Click(object sender, EventArgs e)
+        {
+            string tmp = labcount.Text;
+            int sum = Int32.Parse(tmp)+1;
+            labcount.Text = sum.ToString();
+        }
+
+        private void btncount1_Click(object sender, EventArgs e)
+        {
+            string tmp = labcount1.Text;
+            int sum = Int32.Parse(tmp)-1;
+            labcount1.Text = sum.ToString();
+        }
     }
 }
